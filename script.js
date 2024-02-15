795ae1fc64bd7aa9c0e5c737d9d3891255eda257
Prangam <144879502+Prangam@users.noreply.github.com>
2024-02-16 05:35:58 +0700

    Demo

---
+++ script.js
@@ -0,0 +1,90 @@
+var btnToggle = true;
+var likeBtn = document.getElementById("like");
+var likeBtn2 = document.getElementById("like2");
+var likeBtn3 = document.getElementById("like3");
+var likeBtn4 = document.getElementById("like4");
+var likeBtn5 = document.getElementById("like5");
+var likeBtn6 = document.getElementById("like6");
+var likeBtn7 = document.getElementById("like7");
+var likeBtn8 = document.getElementById("like8");
+
+function fav(){
+    if (btnToggle){
+        likeBtn.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn.style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
+function fav2(){
+    if (btnToggle){
+        likeBtn2.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn2.style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
+function fav3(){
+    if (btnToggle){
+        likeBtn3.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn3.style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
+function fav4(){
+    if (btnToggle){
+        likeBtn4.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn4.style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
+function fav5(){
+    if (btnToggle){
+        likeBtn5.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn5.style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
+function fav6(){
+    if (btnToggle){
+        likeBtn6.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn6.style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
+function fav7(){
+    if (btnToggle){
+        likeBtn7.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn7.style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
+function fav8(){
+    if (btnToggle){
+        likeBtn8.style.backgroundColor = "red";
+        btnToggle = false;
+    } else{
+        likeBtn8  .style.backgroundColor = null;
+        btnToggle = true;
+    }
+}
+
